86aa49719429d4c57c737888727e75d2ae56fede
Polina Haltseva <polinayut@gmail.com>
2025-04-22 15:28:53 +0300

    'Solution'

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
+from car.models import Car
 
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(max_length=64)
+    model = serializers.CharField(max_length=64)
+    horse_powers = serializers.IntegerField(min_value=1, max_value=1914)
+    is_broken = serializers.BooleanField()
+    problem_description = serializers.CharField(
+        required=False,
+        allow_null=True,
+        allow_blank=True,
+    )
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from io import BytesIO
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    validated_data = serializer.validated_data
+    if 'id' in data:
+        validated_data['id'] = data['id']
+    return Car(**validated_data)
